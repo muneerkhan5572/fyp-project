776e4e5536ce98d8d91fff212618adcedb0af502
Muneer Khan <muneerkhan5572@gmail.com>
2026-07-13 16:12:49 +0500

    chore: add forgot password form component and page

---
+++ components/forgot-password-form.tsx
@@ -0,0 +1,53 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Field,
+  FieldDescription,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+
+export function ForgotPasswordForm({
+  ...props
+}: React.ComponentProps<typeof Card>) {
+  return (
+    <Card {...props}>
+      <CardHeader>
+        <CardTitle>Forgot your password?</CardTitle>
+        <CardDescription>
+          Enter your email address and we&apos;ll send you a link to reset your
+          password.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <FieldGroup>
+            <Field>
+              <FieldLabel htmlFor="email">Email</FieldLabel>
+              <Input
+                id="email"
+                placeholder="m@example.com"
+                required
+                type="email"
+              />
+            </Field>
+            <Field>
+              <Button type="submit">Send reset link</Button>
+              <FieldDescription className="text-center">
+                Remember your password? <Link href="/login">Sign in</Link>
+              </FieldDescription>
+            </Field>
+          </FieldGroup>
+        </form>
+      </CardContent>
+    </Card>
+  );
+}
